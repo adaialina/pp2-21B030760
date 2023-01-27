a8012b3e831598da84ff7679053d0b3d9a4d03d4
adaialina <114148082+adaialina@users.noreply.github.com>
2023-01-27 14:42:16 +0600

    Add files via upload

---
+++ tsis1/iv.py
@@ -0,0 +1,6 @@
+print("Hello, World!") 
+#This is my comment, output will be Hello, world!
+"""
+This is a comment
+which takes up more than 1 line
+"""
---
+++ tsis1/vi.py
@@ -0,0 +1,6 @@
+x = complex(1j)
+y = set(("apple", "orange", "banana"))
+z = bool(4)
+print(type(x))
+print(type(y))
+print(type(z))
